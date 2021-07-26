d92aa5f712f85288f8017240482cf82889a9e732
Nguyen Duc <ducnh1@egroup.vn>
2021-07-26 15:03:08 +0700

    Thêm chức năng Thao tác với comment (delete, edit)

--- src/socola/components/text-area/text-area.tsx
+++ src/socola/components/text-area/text-area.tsx
@@ -12,36 +12,11 @@ import { SECRET_KEY } from '../../../config';
 import { removeAllUploadImage, removeUploadImage, setNewFeeds, setNewUploadImage } from '../../../store/action';
 import { UploadImage } from '../../../store/reducer';
 import { FeedType } from '../../../types/feed';
+import { base64ToBlob } from '../../../utils/helper';
 import { Camera } from '../camera';
 import { optionsStatus } from './text-area.data';
 import { TextAreaStyle } from './text-area.style';
 import { TextAreaProps } from './text-area.type';
-
-const b64toBlob = (b64Data: string, contentType = '', sliceSize = 512) => {
-  const byteCharacters = atob(b64Data);
-  const byteArrays = [];
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize);
-    const byteNumbers = new Array(slice.length);
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i);
-    }
-    const byteArray = new Uint8Array(byteNumbers);
-    byteArrays.push(byteArray);
-  }
-  const blob = new Blob(byteArrays, { type: contentType });
-  return blob;
-};
-
-const base64ToBlob = (base64: string) => {
-  const block = base64.split(';');
-  // Get the content type of the image
-  const contentType = block[0].split(':')[1]; // In this case "image/gif"
-  // get the real base64 content of the file
-  const realData = block[1].split(',')[1]; // In this case "R0lGODlhPQBEAPeoAJosM...."
-  // Convert it to a blob to upload
-  return b64toBlob(realData, contentType);
-};
 
 export const TextArea: React.FC<TextAreaProps> = (props) => {
   const { channelId, showDate, showStatus, statusOption, moduleId, recordId } = props;
